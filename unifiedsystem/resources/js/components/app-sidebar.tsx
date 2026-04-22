33f72a7bd0df7faaebc554faaf195d55ad1f48da
Faith <felominofaith@gmail.com>
2026-04-22 16:44:18 +0800

    were so back!!

--- unifiedsystem/resources/js/components/app-sidebar.tsx
+++ unifiedsystem/resources/js/components/app-sidebar.tsx
@@ -22,19 +22,16 @@ const mainNavItems: NavItem[] = [
         href: dashboard(),
         icon: LayoutGrid,
     },
+        {
+        title: 'Dashboard',
+        href: dashboard(),
+        icon: LayoutGrid,
+    },
 ];
 
+
 const footerNavItems: NavItem[] = [
-    {
-        title: 'Repository',
-        href: 'https://github.com/laravel/react-starter-kit',
-        icon: FolderGit2,
-    },
-    {
-        title: 'Documentation',
-        href: 'https://laravel.com/docs/starter-kits#react',
-        icon: BookOpen,
-    },
+
 ];
 
 export function AppSidebar() {

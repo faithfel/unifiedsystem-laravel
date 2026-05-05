0c869e89115eeeb440bc2895d9726e844dcf98d2
Faith <felominofaith@gmail.com>
2026-05-05 19:01:42 +0800

    idk

--- unifiedsystem/resources/js/pages/posterminal.tsx
+++ unifiedsystem/resources/js/pages/posterminal.tsx
@@ -1,13 +1,16 @@
-import { Head } from '@inertiajs/react';
+import { Head, Link } from '@inertiajs/react';
 import { PlaceholderPattern } from '@/components/ui/placeholder-pattern';
 import { posterminal } from '@/routes';
+import { Button } from '@/components/ui/button';
 
-
-export default function POSterminal() {
+export default function PosTerminal() {
     return (
         <>
-            
-
+            <Head title="POS Terminal" />
+            <h1 className='tabtitle'>POS TERMINAL</h1>
+            <div>
+                <Link href={route()}> <Button>Add Product</Button> </Link>
+            </div>
         </>
     );
 }

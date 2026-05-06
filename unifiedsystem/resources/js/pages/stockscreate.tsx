142cefe8c7142d5a2dec21767f207374c15bf767
Faith <felominofaith@gmail.com>
2026-05-06 21:01:28 +0800

    Update stockscreate.tsx

--- unifiedsystem/resources/js/pages/stockscreate.tsx
+++ unifiedsystem/resources/js/pages/stockscreate.tsx
@@ -2,12 +2,20 @@ import { Head, Link } from '@inertiajs/react';
 import { PlaceholderPattern } from '@/components/ui/placeholder-pattern';
 import { stocks, stockscreate } from '@/routes';
 import { Button } from '@/components/ui/button';
+import { Label } from '@radix-ui/react-dropdown-menu';
 
 export default function StocksCreate() {
     return (
         <>
+            <h1 className='tabtitle'>Stock create</h1>
             <Link href={(stocks())}><Button>create</Button></Link>
-
+            
+            <div className=""></div>
+            <form>
+                <div className="">
+                    <Label htmlFor="stocksname"> </Label>
+                </div>
+            </form>
         </>
     );
 }

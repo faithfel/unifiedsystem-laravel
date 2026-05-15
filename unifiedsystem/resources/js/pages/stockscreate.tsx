f20f70d3d7c24820b67b86fba84cf054f3b72ac4
Faith <felominofaith@gmail.com>
2026-05-15 13:28:25 +0800

    css

--- unifiedsystem/resources/js/pages/stockscreate.tsx
+++ unifiedsystem/resources/js/pages/stockscreate.tsx
@@ -28,22 +28,22 @@ export default function StocksCreate() {
             <form>
                 <div className="create-stock-container">
 
-                    <label htmlFor="product">Product</label>
+                    <label htmlFor="product">Product:</label>
                     <input id="product" type="text" name="product" value={data.product}/>
-                    <label htmlFor="sku">SKU</label>
+                    <label htmlFor="sku">SKU:</label>
                     <input id="sku" type="text" name="sku" value={data.sku} />
-                    <label htmlFor="category">Category</label>
+                    <label htmlFor="category">Category:</label>
                     <input id="category" type="text" name="category" value={data.category}/>
-                    <label htmlFor="price">Price</label>
+                    <label htmlFor="price">Price:</label>
                     <input id="price" type="text" name="price" value={data.price} />
-                    <label htmlFor="stock">Stock</label>
+                    <label htmlFor="stock">Stock:</label>
                     <input id="stock" type="text" name="stock" value={data.stock}/>
-                    <label htmlFor="status">Status</label>
+                    <label htmlFor="status">Status:</label>
                         <select id="status" name="status" value={data.status}>
                             <option value="active">Active</option>
                             <option value="inactive">Inactive</option>
                     </select>           
-                    <button type='submit'>Save</button>
+                    <button className='save-button' type='submit'>Save</button>
 
                 </div>
             </form>
